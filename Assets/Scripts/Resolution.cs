eccc1852fe9d082f5abf0b2942137e5b14b799e9
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2022-09-28 14:08:23 -0500

    Resolution Fix

--- Assets/Scripts/Resolution.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Resolution : MonoBehaviour
-{
-    void Start()
-    {
-        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen, 60);
-    }
-}
